bf84439e113651385f9667d0168198d8730d15e7
SpeedyReeky <kokaaomar.10311@gmail.com>
2025-05-13 15:21:33 +0300

    Update Script.js

--- Js/Script.js
+++ Js/Script.js
@@ -1,15 +1,15 @@
 array=["ahmed","mohamed","omar","ali"]
 document.write(`<h1>og array => ${array}`)
 array.push("judy")
-document.write(`<p> push => ${array}<p>`)
+document.write(`<p> push"judy" => ${array}<p>`)
 array.pop()
 document.write(`<p> pop => ${array}<p>`)
 array.unshift("max")
-document.write(`<p> unshift => ${array}<p>`)
+document.write(`<p> unshift "max" => ${array}<p>`)
 array.shift()
 document.write(`<p> shift => ${array}<p>`)
 array.splice(0,2)
-document.write(`<p> splice => ${array}<p>`)
+document.write(`<p> splice (0,2) => ${array}<p>`)
 
 document.write(`<h1>slice => ${array.slice(0,1)}<h1>`)
 
