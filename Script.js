dddcb008efa52c6adf87de5f684bf9d0052c7a5e
SpeedyReeky <kokaaomar.10311@gmail.com>
2025-05-13 15:31:37 +0300

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,21 @@
+array=["ahmed","mohamed","omar","ali"]
+document.write(`<h1>og array => ${array}`)
+array.push("judy")
+document.write(`<p> push => ${array}<p>`)
+array.pop()
+document.write(`<p> pop => ${array}<p>`)
+array.unshift("max")
+document.write(`<p> unshift => ${array}<p>`)
+array.shift()
+document.write(`<p> shift => ${array}<p>`)
+array.splice(0,2)
+document.write(`<p> splice => ${array}<p>`)
+
+document.write(`<h1>slice => ${array.slice(0,1)}<h1>`)
+
+document.write(`<h1>indexOf("ali")=> ${array.indexOf("ali")}<h1>`)
+document.write(`<h1>concat=>  ${array.concat(0,1,"door")}<h1>`)
+array.sort()
+document.write(`<h1>sort=>  ${array}<h1>`)
+array.reverse()
+document.write(`<h1> reverse => ${array}<h1>`)
